55e9ea768e064e22d1f2d81e6f1db78c5ac9f5a7
blitzzz <hung@infinitelambda.com>
2022-11-06 01:08:37 +0700

    try exception

--- macros/auto_snow_mask/custom_masking_policy.sql
+++ macros/auto_snow_mask/custom_masking_policy.sql
@@ -1,4 +1,7 @@
 {% macro custom_masking_policy(masking_policy_name) %}
+declare
+  my_exception exception (-20002, 'Raised MY_EXCEPTION.');
+begin
 
     CREATE OR REPLACE MASKING POLICY {{masking_policy_name}} AS (val string) 
 
@@ -7,5 +10,23 @@
              WHEN CURRENT_ROLE() IN ('SYSADMIN', 'DATA_ENGINEERING') THEN '**custom_masked**'
         ELSE '**********'
         END;
+
+exception
+  when statement_error then
+    return object_construct('Error type', 'STATEMENT_ERROR',
+                            'SQLCODE', sqlcode,
+                            'SQLERRM', sqlerrm,
+                            'SQLSTATE', sqlstate);
+  when my_exception then
+    return object_construct('Error type', 'MY_EXCEPTION',
+                            'SQLCODE', sqlcode,
+                            'SQLERRM', sqlerrm,
+                            'SQLSTATE', sqlstate);
+  when other then
+    return object_construct('Error type', 'Other error',
+                            'SQLCODE', sqlcode,
+                            'SQLERRM', sqlerrm,
+                            'SQLSTATE', sqlstate);
+end;
 
 {% endmacro %}
